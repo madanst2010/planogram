29829d4c3dae4a4fc35e60baace32178c9ea23da
madanst2010 <48178529+madanst2010@users.noreply.github.com>
2021-11-13 16:10:57 +0530

    Update urls.py

--- planogram/urls.py
+++ planogram/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', include("planogramapp.urls")),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
